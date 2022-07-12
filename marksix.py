371d4ba1c49d7fe0a29bb0c3b34443b721e35f6f
garryluk2628 <59395837+garryluk2628@users.noreply.github.com>
2022-07-12 16:05:46 +0800

    Update marksix.py

--- marksix.py
+++ marksix.py
@@ -8,7 +8,7 @@ number = st.number_input(' ')
 #number = st.text_input('Enter how many lines of marksix you want: ')
 number = int(number)
 if number > 1:
-	new_title = '<p style="font-family:Courier; color:Blue; font-size: 40px;">Enter how many lines of marksix you want: </p>',number,'<p style="font-family:Courier; color:Blue; font-size: 40px;"> lines of marksix</p>'
+	new_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">You want to generate  </p>',number,'<p style="font-family:Courier; color:Blue; font-size: 20px;"> lines of marksix</p>'
 	st.markdown(new_title, unsafe_allow_html=True)
 #	st.write('You want to generate ',number,' lines of marksix')
 
