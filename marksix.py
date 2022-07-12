dabada4dbd9bdc3c01bc67d468b0154356d59a58
garryluk2628 <59395837+garryluk2628@users.noreply.github.com>
2022-07-12 16:07:54 +0800

    Update marksix.py

--- marksix.py
+++ marksix.py
@@ -1,16 +1,14 @@
 import random
 import streamlit as st
 
-original_title = '<p style="font-family:Courier; color:Blue; font-size: 40px;">Enter how many lines of marksix you want: </p>'
+original_title = '<p style="font-family:Courier; color:Blue; font-size: 30px;">Enter how many lines of marksix you want: </p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
 number = st.number_input(' ')
 #number = st.text_input('Enter how many lines of marksix you want: ')
 number = int(number)
 if number > 1:
-	new_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">You want to generate  </p>',number,'<p style="font-family:Courier; color:Blue; font-size: 20px;"> lines of marksix</p>'
-	st.markdown(new_title,original_title, unsafe_allow_html=True)
-#	st.write('You want to generate ',number,' lines of marksix')
+	st.write('You want to generate ',number,' lines of marksix')
 
 for i in range(number):
   marksix = list(range(1,50))
