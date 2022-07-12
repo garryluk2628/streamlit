64cc49c83d72e76778f218630706bd9de6e0506f
garryluk2628 <59395837+garryluk2628@users.noreply.github.com>
2022-07-12 15:52:09 +0800

    Update marksix.py

--- marksix.py
+++ marksix.py
@@ -1,7 +1,7 @@
 import random
 import streamlit as st
 
-number = st.number_input('Enter how many lines of marksix you want: ')
+number = st.number_input('#Enter how many lines of marksix you want: ')
 #number = st.text_input('Enter how many lines of marksix you want: ')
 number = int(number)
 if number > 1:
