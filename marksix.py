6bde505007e5c4dd83893bada70c7293ea442f8c
garryluk2628 <59395837+garryluk2628@users.noreply.github.com>
2022-07-12 15:47:50 +0800

    Add files via upload

---
+++ marksix.py
@@ -0,0 +1,15 @@
+import random
+import streamlit as st
+
+number = st.number_input('Enter how many lines of marksix you want: ')
+#number = st.text_input('Enter how many lines of marksix you want: ')
+number = int(number)
+if number > 1:
+	st.write('You want to generate ',number,' lines of marksix')
+
+for i in range(number):
+  marksix = list(range(1,50))
+
+  l = random.sample(marksix,6)
+  l.sort()
+  st.write(l)
