fbee3f47f262a0171b7e1edcad500a6327a5cdcf
garryluk2628 <59395837+garryluk2628@users.noreply.github.com>
2022-07-12 15:59:45 +0800

    Update marksix.py

--- marksix.py
+++ marksix.py
@@ -4,7 +4,7 @@ import streamlit as st
 original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Enter how many lines of marksix you want: </p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
-number = st.number_input()
+number = st.number_input(' ')
 #number = st.text_input('Enter how many lines of marksix you want: ')
 number = int(number)
 if number > 1:
